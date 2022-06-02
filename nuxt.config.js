950919f0fe5fefea004f79c5dd49ea84c28796f5
René Hellenes <rene.hellenes@gmail.com>
2022-06-02 10:38:45 +0200

    Added microanalytics for shits and giggles

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,17 @@ export default {
           'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap',
       },
     ],
+    script: [
+      {
+        src: 'https://microanalytics.io/js/script.js',
+        "data-host": 'https://microanalytics.io',
+        "data-dnt": "false",
+        id: 'ZwSg9rf6GA',
+        async: true,
+        defer:true
+
+      }
+    ]
   },
   server: {
     host: '0.0.0.0', // default: localhost
